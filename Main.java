a1ba1ef790a13ee2fd5bc067bcc21cc110960534
THANDOLISA <tyeyalisa@gmail.com>
2026-08-20 11:39:16 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; // For user input
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String username;
+        String password;
+        String cellPhoneNumber;
+        String Loguser;
+        String Logpassword;
+        String firstName;
+        String lastName;
+
+            System.out.print("Create a username (must contain '_' and be max 5 characters): ");
+            username = scanner.next();
+
+
+
+        }
+}
