6a316b5dcf8f9b018cfc5525d7b16d82fc6487c9
THANDOLISA <tyeyalisa@gmail.com>
2026-08-20 11:41:28 +0200

    Add main method and user input variables

--- Main.java
+++ Main.java
@@ -3,3 +3,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String username;
+        String password;
+ 
